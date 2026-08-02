6ae209fc8044573b04358d713a8e4f553cf9177a
峰哥 <362053534@qq.com>
2026-08-03 03:02:33 +0800

    重定向libsmb2到自己仓库  避免缓存

--- build-cmakelibs.sh
+++ build-cmakelibs.sh
@@ -108,7 +108,7 @@ $FETCH release-2.6.3 https://github.com/libsdl-org/SDL_mixer.git &
 $FETCH release-2.8.4 https://github.com/libsdl-org/SDL_image.git &
 $FETCH release-2.24.0 https://github.com/libsdl-org/SDL_ttf.git &
 
-$FETCH 6.2 https://github.com/362053534/libsmb2.git &
+$FETCH d86e444e87e8f5faf59b2ce7e2092185ae4f413a https://github.com/362053534/libsmb2.git &
 
 # We need to clone the whole repo and point to the specific hash for now,
 # till a new version is released after this commit
